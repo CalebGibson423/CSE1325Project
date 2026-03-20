52fdcebe6e53c72c2eb27bda46b5b0753a97e6d8
Kennedyrc1201 <114550263+Kennedyrc1201@users.noreply.github.com>
2026-03-20 15:40:19 -0500

    Update Main.java
    Testing commit

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
-package CSE1325Project;
-public class Main {
-  public static void main(String[] args) {
-    String name = "John";
-    System.out.println("Hello " + name);
-  }
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+
+public static void main(String[] args)
+{
+  Scanner input = new Scanner(System.in);
+
+  LinkedList<Event> events = new LinkedList<Event>();
+
+  String event_name = "marathon";
+  int start_day = 23;
+  int start_month = 4;
+  int start_year = 2026;
+  double duration = 4.50;
+  ArrayList<String> event_types = "exercise"
+
+  Event(event_name, start_day, start_month, start_year, duration, event_types);
+  System.out.print(events);
+  // Event event = ???
+  // events.add(new Event(event_name, event_type, ...))
 }
