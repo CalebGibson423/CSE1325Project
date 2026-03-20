b5e854366d3af2a0e4ac1c2f66567768ff792976
Kennedyrc1201 <114550263+Kennedyrc1201@users.noreply.github.com>
2026-03-20 16:41:03 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,21 +2,29 @@ import java.util.*;
 import java.time.*;
 import java.time.format.*;
 
-public static void main(String[] args)
+public class Main
 {
-  Scanner input = new Scanner(System.in);
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
 
-  LinkedList<Event> events = new LinkedList<Event>();
+    LinkedList<Event> events = new LinkedList<Event>();
 
-  String event_name = "marathon";
-  int start_day = 23;
-  int start_month = 4;
-  int start_year = 2026;
-  double duration = 4.50;
-  ArrayList<String> event_types = "exercise"
-
-  Event(event_name, start_day, start_month, start_year, duration, event_types);
-  System.out.print(events);
-  // Event event = ???
-  // events.add(new Event(event_name, event_type, ...))
+    String event_name = "Marathon";
+    int start_day = 23;
+    int start_month = 4;
+    int start_year = 2026;
+    double duration = 4.50;
+    ArrayList<String> event_types = new ArrayList<>();
+    String format = "Online";        
+    event_types.add("Educational");
+    event_types.add("Social");
+    event_types.add("Gaming");
+    event_types.add("Alcohol");
+    
+    // Event event = ???
+    events.add(new Event(event_name, start_day, start_month, start_year, duration, event_types, format));
+    System.out.println(events.get(0));
+    
+  }
 }
