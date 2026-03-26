98a9c6bb02bffa2e4a354c681aed9fd92e328afb
Kennedyrc1201 <114550263+Kennedyrc1201@users.noreply.github.com>
2026-03-26 19:11:54 +0000

    Added comments to FileManager

--- FileManager.java
+++ FileManager.java
@@ -5,11 +5,16 @@ import java.time.*;
 
 public class FileManager {
 
-    public static void saveEvents(LinkedList<Event> events){
-        
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("events.txt"));) {
-
-            for(int i = 0; i < events.size(); i++){
+    //Write's the list of events to a file
+    public static void saveEvents(LinkedList<Event> events)
+    {
+        //Try Open/Create a file called "events.txt"
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("events.txt"));) 
+        {
+            //Loop through events
+            for(int i = 0; i < events.size(); i++)
+            {
+                //For each event, write event info in the following format
                 writer.write(
                     events.get(i).getName() + "/" +
                     events.get(i).getDate().format(Constants.dateFormatter) + "/" +
@@ -19,46 +24,63 @@ public class FileManager {
                     events.get(i).getFormat()
                 );
 
+                //Move to next line for the next event
                 writer.newLine();
             }
 
-        } catch (IOException e) {
+        } 
+        //If something goes wrong
+        catch (IOException e) 
+        {
             System.out.println("An error occurred: " + e.getMessage());
         }
     }
 
-    public static ArrayList<Event> loadEvents(){
-
-        try (BufferedReader reader = new BufferedReader(new FileReader("events.txt"))) {
-
+    //Read events from a file
+    public static ArrayList<Event> loadEvents()
+    {
+        //Try open file
+        try (BufferedReader reader = new BufferedReader(new FileReader("events.txt"))) 
+        {
+            
             ArrayList<Event> events = new ArrayList<Event>();
 
             String line;
-            while ((line = reader.readLine()) != null) {
-                
+            //Reads the file line by line
+            while ((line = reader.readLine()) != null) 
+            {
+                //Splits the line (event info)
                 String[] eventParts = line.split("/");
 
+                //Put event info into proper types
                 String name = eventParts[0];
                 LocalDate date = LocalDate.parse(eventParts[1], Constants.dateFormatter);
                 LocalTime time = LocalTime.parse(eventParts[2], Constants.timeFormatter);
                 double duration = Double.parseDouble(eventParts[3]);
 
+                //Handles the "types" list and puts all types into an ArrayList
                 String[] typesArray = eventParts[4].split(",");
                 ArrayList<String> typesList = new ArrayList<>();
-
-                for(int i = 0; i < typesArray.length; i++){
+                //Adds each type to the ArrayList
+                for(int i = 0; i < typesArray.length; i++)
+                {
                     typesList.add(typesArray[i]);
                 }
-              
+
+                //Puts event format into String
                 String format = eventParts[5];
 
-                Event newEvent = new Event(name, date, time, duration, typesList, format);
-                events.add(newEvent);
+                //Creates new event object
+                Event newEvent = new Event(name, date, time, duration, typesList, format); 
+                events.add(newEvent);                   //Adds the event read from the line to a list
             }
 
-            return events;
+            return events;              //Returns event list
 
-        } catch (IOException e) {
+        } 
+        //If file fails
+        catch (IOException e) 
+        {
             System.out.println("An error occurred: " + e.getMessage());
             return new ArrayList<>();
         }
